226ed43a4e20b50a992c702ed1b20bd0f93d81b9
rmartind <rmdavis@buffalo.edu>
2019-02-20 03:53:07 -0500

    Add upload to mongo collection. Work on rating ability.

--- hot_or_not/main.py
+++ hot_or_not/main.py
@@ -1,19 +1,35 @@
 import discord
 import random
 import asyncio
-import pymongo
 import configparser
+from pymongo import MongoClient
+import pymongo
 from imgur import Imgur
+from collections import OrderedDict
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 imgur_client_id = config['IMGUR_CLIENT']['CLIENT_ID']
-girl_album_hash = config['IMGUR_CLIENT']['GIRL_HASH']
-
 imgur = Imgur(imgur_client_id)
+
 client = discord.Client()
+
+mongo_client = MongoClient('mongodb://127.0.0.1:27017/')
+db = mongo_client.hotornot
+girl_collection = db.girls
+
+rating_cache = OrderedDict()
+
+def create_embed(link, rating, msg):
+    embed = discord.Embed(
+        title='*How to rate*', description='`.rate 1-10 to rate this image`', color=0x00BFFF, timestamp=msg.timestamp)
+    embed.set_author(name='Hot or Not')
+    embed.add_field(name='Average rating:', value=rating, inline=True)
+    embed.set_image(url=link)
+    embed.set_footer(text='sent')
+    return embed
 
 
 @client.event
@@ -23,32 +39,68 @@ async def on_ready():
 
 @client.event
 async def on_message(msg):
-    if msg.channel.id == '502656152702681089':
+    if msg.channel.id == config['DISCORD_CLIENT']['BOT_CHANNEL']:
         if msg.content.startswith('.girl'):
 
-            girl_images = imgur.album_images(girl_album_hash)['data']
-            girl_count = len(girl_images)
-            random_girl = girl_images[random.randrange(girl_count)]
+           
+            count = girl_collection.count()
+            number = random.randrange(count)
+            image = girl_collection.find().limit(1).skip(number)
 
+            hash = None
+            url = None
+            rating = None
+            for e in image:
+                url = e['link']
+                rating = e['rating']
+                hash = e['_id']
 
-            embed = discord.Embed(
-                title='*How to rate*', description='`.rate 1-10 to rate this image`', color=0x00BFFF, timestamp=msg.timestamp)
-            embed.set_author(name='Hot or Not')
-            embed.add_field(name='Average rating:', value=2.5, inline=True)
-            embed.set_image(url=random_girl['link'])
-            embed.set_footer(text='sent')
+            if len(rating_cache) < 4:
+                rating_cache[msg.author.id] = hash
+            else:
+                rating_cache.popitem()
+
+            embed = create_embed(url, rating, msg)
             await client.send_message(msg.channel, embed=embed)
 
         elif msg.content.startswith('.boy'):
-            embed = discord.Embed(
-                title='*How to rate*', description='`.rate 1-10 to rate this image`', color=0x00BFFF, timestamp=msg.timestamp)
-            embed.set_author(name='Hot or Not')
-            embed.add_field(name='Average rating:', value=2.5, inline=True)
-            embed.set_image(url='https://i.imgur.com/3vW8pdd.jpg')
-            embed.set_footer(text='sent')
-            await client.send_message(msg.channel, embed=embed)
+            pass
 
         elif msg.content.startswith('.rate'):
-            print('.rate')
+            try: 
+                hash = rating_cache[msg.author.id]
+                image = girl_collection.find_one({'_id': hash})
+                print(image)
 
-client.run('NTQ3MjM4MTIyMjI0NzQ2NDk2.D05Ubg.UP4NZKpH71VBWaLdAGvzbGYGMU0')
+
+                del rating_cache[msg.author.id]
+                await client.send_message(msg.channel, 'Rating added.')
+
+            except KeyError:
+                await client.send_message(msg.channel, 'Nothing to rate')
+              
+
+        elif msg.content.startswith('.upload album'):
+            if msg.author.id == '537741349961859099':
+                hash = msg.content[len('.upload album'):].strip()
+                images = imgur.album_images(hash)['data']
+                for image in images:
+                    girl = {
+                        '_id': image['id'],
+                        'rating': 0.0,
+                        'count': 0,
+                        'link': image['link']
+                    }
+
+                    try:
+                        girl_collection.insert_one(girl)
+                    except pymongo.errors.DuplicateKeyError:
+                        continue
+            else:
+                await client.send_message(msg.channel, 'You must be an administator to perform this command.')
+        elif msg.content.startswith('.upload image'):
+            if msg.author.id == '537741349961859099':
+                pass
+
+
+client.run(config['DISCORD_CLIENT']['BOT_TOKEN'])
