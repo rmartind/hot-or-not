e6016fb07edebc5b24bb64846202490fa52644d7
rmartind <rmdavis@buffalo.edu>
2019-02-21 00:35:59 -0500

    Seperate mongo into seperate class. Other things.
    Refactor database code into its own class. Add
    uploading and rating functionality for men. Add
    a help command. Add icon url, vote count to rich embed
    and two precision Decimal averaging. Change rich embed color
    to red. Modularize rating code in main

--- hot_or_not/main.py
+++ hot_or_not/main.py
@@ -1,106 +1,117 @@
 import discord
-import random
 import asyncio
 import configparser
-from pymongo import MongoClient
-import pymongo
-from imgur import Imgur
 from collections import OrderedDict
+import pymongo
+from mongo import Mongo
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-imgur_client_id = config['IMGUR_CLIENT']['CLIENT_ID']
-imgur = Imgur(imgur_client_id)
-
+mongo = Mongo(config['IMGUR_CLIENT']['CLIENT_ID'])
 client = discord.Client()
-
-mongo_client = MongoClient('mongodb://127.0.0.1:27017/')
-db = mongo_client.hotornot
-girl_collection = db.girls
-
 rating_cache = OrderedDict()
 
-def create_embed(link, rating, msg):
+
+def create_embed(link, rating, count, msg):
     embed = discord.Embed(
-        title='*How to rate*', description='`.rate 1-10 to rate this image`', color=0x00BFFF, timestamp=msg.timestamp)
-    embed.set_author(name='Hot or Not')
+        title='*How to rate*', description='`.rate 1-10 to rate this image`', color=0xff3232, timestamp=msg.timestamp)
+    embed.set_author(name='Hot or Not', icon_url=client.user.avatar_url)
     embed.add_field(name='Average rating:', value=rating, inline=True)
+    embed.add_field(name='Votes:', value=count, inline=True)
     embed.set_image(url=link)
-    embed.set_footer(text='sent')
     return embed
+
+def help_embed(msg):
+    embed = discord.Embed(
+        title='*Instructions*', description='`displays photos of boys and girls to rate.`', color=0xff3232, timestamp=msg.timestamp)
+    embed.set_author(name='Hot or Not', icon_url=client.user.avatar_url)
+    embed.add_field(name='Show girl:', value='`.girl`', inline=True)
+    embed.add_field(name='Show boy:', value='`.boy`', inline=True)
+    embed.add_field(name='rate:', value='`.rate value - value must be 1-10`', inline=True)
+    embed.set_thumbnail(url=msg.channel.server.icon_url)
+    return embed    
+
+def add_rating(new_rating, requester):
+    if new_rating <= 10 and new_rating > 0:
+        if rating_cache[requester][1] == 'girl':
+            mongo.update_girl(rating_cache[requester][0], new_rating)
+        else:
+            mongo.update_boy(rating_cache[requester][0], new_rating)
+        del rating_cache[requester]
+    else:
+        raise ValueError('Rating must be between 0-10.')
 
 
 @client.event
 async def on_ready():
     print(client.user.name)
+    await client.change_presence(game=discord.Game(name='.help'), status=discord.Status.dnd)
 
 
 @client.event
 async def on_message(msg):
     if msg.channel.id == config['DISCORD_CLIENT']['BOT_CHANNEL']:
         if msg.content.startswith('.girl'):
-
-           
-            count = girl_collection.count()
-            number = random.randrange(count)
-            image = girl_collection.find().limit(1).skip(number)
-
+            image = mongo.random_girl()
             hash = None
             url = None
             rating = None
+            count = None
             for e in image:
                 url = e['link']
                 rating = e['rating']
                 hash = e['_id']
+                count = e['count']
 
             if len(rating_cache) < 4:
-                rating_cache[msg.author.id] = hash
+                rating_cache[msg.author.id] = (hash, 'girl')
             else:
-                rating_cache.popitem()
+                rating_cache.popitem(last=False)
 
-            embed = create_embed(url, rating, msg)
+            embed = create_embed(url, rating, count, msg)
             await client.send_message(msg.channel, embed=embed)
-
         elif msg.content.startswith('.boy'):
-            pass
+            image = mongo.random_boy()
+            hash = None
+            url = None
+            rating = None
+            count = None
+            for e in image:
+                url = e['link']
+                rating = e['rating']
+                hash = e['_id']
+                count = e['count']
 
-        elif msg.content.startswith('.rate'):
-            try: 
-                hash = rating_cache[msg.author.id]
-                image = girl_collection.find_one({'_id': hash})
-                print(image)
-
-
-                del rating_cache[msg.author.id]
-                await client.send_message(msg.channel, 'Rating added.')
-
-            except KeyError:
-                await client.send_message(msg.channel, 'Nothing to rate')
-              
-
-        elif msg.content.startswith('.upload album'):
-            if msg.author.id == '537741349961859099':
-                hash = msg.content[len('.upload album'):].strip()
-                images = imgur.album_images(hash)['data']
-                for image in images:
-                    girl = {
-                        '_id': image['id'],
-                        'rating': 0.0,
-                        'count': 0,
-                        'link': image['link']
-                    }
-
-                    try:
-                        girl_collection.insert_one(girl)
-                    except pymongo.errors.DuplicateKeyError:
-                        continue
+            if len(rating_cache) < 4:
+                rating_cache[msg.author.id] = (hash, 'boy')
             else:
-                await client.send_message(msg.channel, 'You must be an administator to perform this command.')
-        elif msg.content.startswith('.upload image'):
-            if msg.author.id == '537741349961859099':
-                pass
+                rating_cache.popitem(last=False)
+            embed = create_embed(url, rating, count, msg)
+            await client.send_message(msg.channel, embed=embed)
+    
+        elif msg.content.startswith('.rate'):
+            try:
+                add_rating(int(msg.content[len('.rate'):].strip()), msg.author.id)
+                await client.send_message(msg.channel, '`Rating added.`')
+            except KeyError:
+                await client.send_message(msg.channel, '`Nothing to rate.`')
+            except ValueError:
+                await client.send_message(msg.channel, '`Rating must be between 0-10.`')
+
+        elif msg.content.startswith('.upload album girl'):
+            if msg.author.id == config['DISCORD_CLIENT']['ADMIN_ID']:
+                mongo.upload_album(msg.content[len('.upload album girl'):].strip())
+            else:
+                await client.send_message(msg.channel, '`You must be an administator to perform this command.`')
+
+        elif msg.content.startswith('.upload album boy'):
+            if msg.author.id == config['DISCORD_CLIENT']['ADMIN_ID']:
+                mongo.upload_album(msg.content[len('.upload album boy'):].strip(), girl=False)
+            else:
+                await client.send_message(msg.channel, '`You must be an administator to perform this command.`')
+        elif msg.content.startswith('.help'):
+            await client.send_message(msg.channel, embed=help_embed(msg))
 
 
 client.run(config['DISCORD_CLIENT']['BOT_TOKEN'])
---
+++ hot_or_not/mongo.py
@@ -0,0 +1,65 @@
+import pymongo
+import random
+import asyncio
+from decimal import Decimal
+from pymongo import MongoClient
+from imgur import Imgur
+
+
+class Mongo(object):
+    def __init__(self, imgur_id):
+        self._imgur_client_id = imgur_id
+        self._imgur = Imgur(self._imgur_client_id)
+        self._client = MongoClient('mongodb://127.0.0.1:27017/')
+        self._db = self._client.hotornot
+
+    def random_girl(self):
+        return self._db.girls.find().limit(1) \
+            .skip(random.randrange(self._db.girls.count()))
+
+    def random_boy(self):
+        return self._db.boys.find().limit(1) \
+            .skip(random.randrange(self._db.boys.count()))
+
+    def upload_album(self, hash, girl=True):
+        images = self._imgur.album_images(hash)['data']
+        for image in images:
+            doc = {
+                '_id': image['id'],
+                'rating': 0.0,
+                'count': 0,
+                'link': image['link']
+            }
+
+            try:
+                if girl:
+                    self._db.girls.insert_one(doc)
+                else:
+                    self._db.boys.insert_one(doc)
+            except pymongo.errors.DuplicateKeyError:
+                continue
+
+    def upload_image(self, hash, girl=True):
+        pass
+
+    def update_girl(self, hash, new_rate):
+        image = self._db.girls.find_one({'_id': hash})
+        total_average = self.average(image['rating'], new_rate, image['count'])
+
+        self._db.girls.find_one_and_update(
+            {'_id': hash}, {'$inc': {'count': 1},
+                            '$set': {'rating': total_average}},
+            return_document=pymongo.ReturnDocument.AFTER)
+
+    def update_boy(self, hash, new_rate):
+        image = self._db.boys.find_one({'_id': hash})
+        total_average = self.average(image['rating'], new_rate, image['count'])
+
+        self._db.boys.find_one_and_update(
+            {'_id': hash}, {'$inc': {'count': 1},
+                            '$set': {'rating': total_average}},
+            return_document=pymongo.ReturnDocument.AFTER)
+
+    @staticmethod
+    def average(old_rating, new_rating, count):
+        return float(round(Decimal((old_rating * count + new_rating) / (count + 1)),1))
